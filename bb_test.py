c079e04366c7ff8107a8fefdd42ec578973ad7a5
jami1992 <jan-michael.druba@mailbox.tu-dresden.de>
2020-05-26 13:28:52 +0200

    improved explanations

--- bb_test.py
+++ bb_test.py
@@ -26,24 +26,23 @@ bot_auth_id = open("../../bb_id/bb_config.txt").read()
 
 # this function returns a funny german toast. try to pronounce it correctly.
 def toast():
+    # get the number of lines of the toast.txt-file
     num_lines = sum(1 for line in open('toast.txt'))
+    # open the file with the funny german toasts
     toast_file = open('toast.txt')
     lines = toast_file.readlines()
     # in the lines statement, 0 represents the first line of the toast.txt-file.
     # That's why we need a random integer between 0 and num_lines-1
     # we have to use iso-8859 to avoid problems with german umalauts. .decode function is necessary to interpret
     # backslashes
-    #zufallszahl = random.randint(0, num_lines - 1)
-    # problemzeile ausgeben
-    #print(zufallszahl+1)
     funny_german_toast = bytes(lines[random.randint(0, num_lines - 1)], "iso-8859-1").decode("unicode_escape") # python3)
-    # the print statement prints the toast on the server's command-line. It's can be commented out, later
-    #print(funny_german_toast)
+    # the print statement would print the toast to the server's command-line.
+    print(funny_german_toast)
     # this statement returns the funny german toast to the text interpreter function.
     # the text interpreter function sends it to the telegram-user (the beer drinker)
     return funny_german_toast
 
-# This function does the database entrys (in earlier development-stage it will only write to a file called log.txt
+# This function does the database entries (in earlier development-stage it will only write to a file called log.txt
 def db_entry(user_ID, user_FIRSTNAME, berlin_date, berlin_time, amount):
     log_entry = str(user_ID) + ";" + str(user_FIRSTNAME) + ";" + str(berlin_date) + ";" + str(
         berlin_time) + ";" + amount + ";" + "\n"
@@ -57,8 +56,9 @@ def help(update, context):
     update.message.reply_text(
         "[english version below]\n schreibe /register und drücke auf senden. Dann wähle die gezapfte Größe aus.")
 
-# die Funktion text_interpreter verarbeitet den text
+# the function text interpreter handles inputs of the user
 def text_interpreter(update, context):
+    # variables for info about the user and the time
     user = update.message.from_user
     user_ID = format(user['id'])
     user_FIRSTNAME = format(user['first_name'])
@@ -66,6 +66,7 @@ def text_interpreter(update, context):
     berlin_date = datetime.now(berlin).strftime("%d.%m.%y")
     berlin_time = datetime.now(berlin).strftime("%X")
     received_message = update.message.text
+    # handling of user inputs regarding the amounts of beer they took:
     if received_message == '0,1l  \n 🍺':
         db_entry(user_ID, user_FIRSTNAME, berlin_date, berlin_time, '0.1')
         # lets get a funny german toast and send it to the users device:
@@ -86,13 +87,13 @@ def text_interpreter(update, context):
         update.message.reply_text(
             "Du hast '" + received_message + "' geschrieben. Damit kann ich leider nix anfangen. Gib /help ein und ich sag dir was ich verstehe...")
 
-# die custom-keyboard Funktion
+# the custom-keyboard function
 def custom_keyboard(update, context):
     keyboard = [["0,1l  \n 🍺", "0,2l \n 🍺🍺"], ["0,4l \n 🍺🍺 \n 🍺🍺", "1l \n 🍺🍺🍺🍺🍺 \n🍺🍺🍺🍺🍺"]]
     reply_markup = telegram.ReplyKeyboardMarkup(keyboard)
     update.message.reply_text(text="Wieviel?", reply_markup=reply_markup)
 
-# die "Master-Funktion"
+# the main function does only call the other functions. Programming should be done outside (above)
 def main():
     updater = Updater(bot_auth_id, use_context=True)
     # dispatcher versendet infos an server:
@@ -119,7 +120,6 @@ def main():
     updater.idle()
 
     # Python files can act as either reusable modules, or as standalone programs.
-
 
 if __name__ == '__main__':
     main()
