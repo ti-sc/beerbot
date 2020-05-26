a69b68afc139451978facc7939af1a8387c46a36
jami1992 <49276140+jami1992@users.noreply.github.com>
2020-05-26 03:05:05 +0200

    Add files via upload
    clean functions for txt-entries & new feature "funny german toasts"

--- bb_test.py
+++ bb_test.py
@@ -2,9 +2,12 @@
 # 1. Telegram-Bot-Library importieren:
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
+import random
 from telegram import ReplyKeyboardMarkup
 from datetime import datetime
 import pytz
+import sqlite3
+
 # updater holt sich die updates vom bot-nutzer
 # commandhandler verarbeitet alles mit slash davor, also alle Befehle
 # Messagehandler verarbeitet alles was kein Befehl ist
@@ -13,15 +16,43 @@ import pytz
 # liest die Bot-ID aus, über die der Bot gesteuert werden kann.
 bot_auth_id = open("../../bb_id/bb_config.txt").read()
 
+# connection mit sqlite datenbank aufbauen:
+conn = sqlite3.connect('bierbot_datenbank.db')
+
+
 # in der Funktion hier werden die Parameter update zum updaten und context
 # zum verwenden von Daten von außerhalb der Funktion übergeben.
-def start(update, context):
-    # der folgende Befehl antwortet an den Nutzer:
-    update.message.reply_text("Bitte trag dein Bier ein: ")
+
+# this function returns a funny german toast. try to pronounce it correctly.
+def toast():
+    num_lines = sum(1 for line in open('toast.txt'))
+    toast_file = open('toast.txt')
+    lines = toast_file.readlines()
+    # in the lines statement, 0 represents the first line. That's why we need a random integer between 0 and num_lines-1
+    # we have to use iso-8859 to avoid problems with german umalauts. .decode function is necessary to interpret
+    # backslashes
+    zufallszahl = random.randint(0, num_lines - 1)
+    # problemzeile ausgeben
+    print(zufallszahl+1)
+    rueckgabewert = bytes(lines[zufallszahl], "iso-8859-1").decode("unicode_escape") # python3)
+    print(rueckgabewert)
+    return rueckgabewert
+
+# This function does the database entrys (in earlier development-stage it will only write to a file called log.txt
+def db_entry(user_ID, user_FIRSTNAME, berlin_date, berlin_time, amount):
+    log_entry = str(user_ID) + ";" + str(user_FIRSTNAME) + ";" + str(berlin_date) + ";" + str(
+        berlin_time) + ";" + amount + ";" + "\n"
+    print(log_entry)
+    text_file = open("../log.txt", "a")
+    n = text_file.write(log_entry)
+    text_file.close()
+
 
 # muss noch an custom-keyboards angepasst werden...
 def help(update, context):
-    update.message.reply_text("[english version below]\n schreibe /register und drücke auf senden. Dann wähle die gezapfte Größe aus.")
+    update.message.reply_text(
+        "[english version below]\n schreibe /register und drücke auf senden. Dann wähle die gezapfte Größe aus.")
+
 
 # die Funktion text_interpreter verarbeitet den text
 def text_interpreter(update, context):
@@ -31,56 +62,48 @@ def text_interpreter(update, context):
     berlin = pytz.timezone('Europe/Berlin')
     berlin_date = datetime.now(berlin).strftime("%d.%m.%y")
     berlin_time = datetime.now(berlin).strftime("%X")
-    print(berlin_date)
-    print(berlin_time)
     received_message = update.message.text
     if received_message == '0,1l  \n 🍺':
-        update.message.reply_text("Das nenne ich mal verantwortungsbewussten Alkoholkonsum")
-        log_entry = str(user_ID) + ";" + str(user_FIRSTNAME) + ";" + str(berlin_date) + ";" + str(berlin_time) + ";" + "0.1" + ";" + "\n"
-        print(log_entry)
-        text_file = open("../log.txt", "a")
-        n = text_file.write(log_entry)
-        text_file.close()
+        db_entry(user_ID, user_FIRSTNAME, berlin_date, berlin_time, '0.1')
+        # lets get a funny german toast and send it to the users device:
+        update.message.reply_text(toast())
     elif received_message == '0,2l \n 🍺🍺':
-        update.message.reply_text("Misch noch Sprite o.Ä. dazu, dann hast du ein volles Glas Radler")
-        log_entry = str(user_ID) + ";" + str(user_FIRSTNAME) + ";" + str(berlin_date) + ";" + str(berlin_time) + ";" + "0.2" + ";" + "\n"
-        print(log_entry)
-        text_file = open("../log.txt", "a")
-        n = text_file.write(log_entry)
-        text_file.close()
+        db_entry(user_ID, user_FIRSTNAME, berlin_date, berlin_time, '0.2')
+        # lets get a funny german toast and send it to the users device:
+        update.message.reply_text(toast())
     elif received_message == '0,4l \n 🍺🍺 \n 🍺🍺':
-        update.message.reply_text("Standard")
-        log_entry = str(user_ID) + ";" + str(user_FIRSTNAME) + ";" + str(berlin_date) + ";" + str(berlin_time) + ";" + "0.4" + ";" + "\n"
-        print(log_entry)
-        text_file = open("../log.txt", "a")
-        n = text_file.write(log_entry)
-        text_file.close()
+        db_entry(user_ID, user_FIRSTNAME, berlin_date, berlin_time, '0.4')
+        # lets get a funny german toast and send it to the users device:
+        update.message.reply_text(toast())
     elif received_message == '1l \n 🍺🍺🍺🍺🍺 \n🍺🍺🍺🍺🍺':
-        update.message.reply_text("Trink nicht zuviel davon")
-        log_entry = str(user_ID) + ";" + str(user_FIRSTNAME) + ";" + str(berlin_date) + ";" + str(berlin_time) + ";" + "1.0" + ";" + "\n"
-        print(log_entry)
-        text_file = open("../log.txt", "a")
-        n = text_file.write(log_entry)
-        text_file.close()
+        db_entry(user_ID, user_FIRSTNAME, berlin_date, berlin_time, '1.0')
+        # lets get a funny german toast and send it to the users device:
+        update.message.reply_text(toast())
     else:
-        update.message.reply_text("Du hast '" + received_message + "' geschrieben. Damit kann ich leider nix anfangen. Gib /help ein und ich sag dir was ich verstehe...")
+        update.message.reply_text(
+            "Du hast '" + received_message + "' geschrieben. Damit kann ich leider nix anfangen. Gib /help ein und ich sag dir was ich verstehe...")
+
 
 # so später nicht notwendig, aber erstmal hilfreich um zu verstehen wie username usw abgerufen werden können.
-def deinname(update,context):
+def deinname(update, context):
     user = update.message.from_user
     user_ID = format(user['id'])
     user_NAME = format(user['username'])
     user_FIRSTNAME = format(user['first_name'])
-    user_LASTNAME = format(user['last_name'])  # die bequeme Variante user['full_name'] funktioniert aus irgendeinem Grund nicht.
-    #user_FULLNAME = format(user['full_name'])
-    print("You talk with " + user_NAME + " his/her user ID is " + user_ID + " his/her real name is " + user_FIRSTNAME + " " + user_LASTNAME)
+    user_LASTNAME = format(
+        user['last_name'])  # die bequeme Variante user['full_name'] funktioniert aus irgendeinem Grund nicht.
+    # user_FULLNAME = format(user['full_name'])
+    print(
+        "You talk with " + user_NAME + " his/her user ID is " + user_ID + " his/her real name is " + user_FIRSTNAME + " " + user_LASTNAME)
     update.message.reply_text(user_FIRSTNAME + " du solltest deinen Alkoholkonsum überdenken!")
+
 
 # die custom-keyboard Funktion
 def custom_keyboard(update, context):
-    keyboard = [[ "0,1l  \n 🍺", "0,2l \n 🍺🍺"], ["0,4l \n 🍺🍺 \n 🍺🍺", "1l \n 🍺🍺🍺🍺🍺 \n🍺🍺🍺🍺🍺" ]]
+    keyboard = [["0,1l  \n 🍺", "0,2l \n 🍺🍺"], ["0,4l \n 🍺🍺 \n 🍺🍺", "1l \n 🍺🍺🍺🍺🍺 \n🍺🍺🍺🍺🍺"]]
     reply_markup = telegram.ReplyKeyboardMarkup(keyboard)
-    update.message.reply_text(text = "Wieviel?", reply_markup = reply_markup)
+    update.message.reply_text(text="Wieviel?", reply_markup=reply_markup)
+
 
 # die "Master-Funktion"
 def main():
@@ -92,8 +115,7 @@ def main():
     # handler teilt den Befehlen / dem Text der bei Telegram eingegeben wird Funktionen zu
     # Beispiel-Handler: in Telegram wird der Befehl /start eingegeben
     # ausgeführt wird dann in diesem Skript die Funktion, die dem Befehl zugeteilt wird
-    # hier als Beispiel wird der Befehl start mit der Funktion start assoziiert.
-    dp.add_handler(CommandHandler("start", start))
+
     # Help-Handler (erklärt wie man sich sein Bier einträgt)
     dp.add_handler(CommandHandler("help", help))
 
@@ -113,5 +135,7 @@ def main():
     updater.idle()
 
     # Python files can act as either reusable modules, or as standalone programs.
-if __name__=='__main__':
-        main()
+
+
+if __name__ == '__main__':
+    main()
